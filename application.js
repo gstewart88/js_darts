c26235e42362a00825554184135b18674e36ef13
Graeme <flangemodo@hotmail.com>
2015-11-30 09:41:09 +0000

    add score to page

--- application.js
+++ application.js
@@ -4,21 +4,23 @@ var player2score = start;
 var in1 = document.getElementById('darts');
 var in2 = document.getElementById('darts2');
 
-$('#input').submit(function(e) {
-  e.preventDefault();
-  var darts = parseInt(in1.value)
-  player1score = (player1score - darts)
-  $('#player1').append('<li>'+(player1score)+'</li>');
+document.getElementById("startscore").innerHTML = (start);
+
+  $('#input').submit(function(e) {
+    e.preventDefault();
+    var darts = parseInt(in1.value)
+    player1score = (player1score - darts)
+    $('#player1').append('<li>'+(player1score)+'</li>');
   // success(clearForm);
 });
 
-$('#input2').submit(function(e) {
-  e.preventDefault();
-  var darts2 = parseInt(in2.value)
-  player2score = (player2score - darts2)
-  $('#player2').append('<li>'+(player2score)+'</li>');
-});
+  $('#input2').submit(function(e) {
+    e.preventDefault();
+    var darts2 = parseInt(in2.value)
+    player2score = (player2score - darts2)
+    $('#player2').append('<li>'+(player2score)+'</li>');
+  });
 
-function clearForm() {
-  $('#number').val('');
-}
+  function clearForm() {
+    $('#number').val('');
+  }
